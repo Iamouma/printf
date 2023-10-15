196e9b2fe67a81535b98c3e01647d84693b84ff0
Iamouma <stephenouma452@gmail.com>
2023-10-15 01:59:36 -0700

    Add files

---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
